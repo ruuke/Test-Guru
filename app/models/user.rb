2e5218f144be2f7ad07b19ca60bfaca475dc1bc5
ruuke <ruuke@mail.ru>
2018-11-08 08:38:12 +0500

    refactoring models defs

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   def tests_passage(level)
     Test
       .joins('JOIN test_passages ON tests.id = test_passages.test_id')
-      .where('test_passages.user_id = ?', id)
-      .where('tests.level = ?', level)
+      .where(test_passages: {user_id: id})
+      .where(level: level)
   end
 end
