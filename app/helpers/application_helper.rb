7bbaa4d2dae5589c011314f95a6aa769a1461b32
ruuke <ruuke@mail.ru>
2019-02-07 17:22:20 +0500

    bootstrap

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,13 @@ module ApplicationHelper
   def github_url(author, repo)
     "https://github.com/#{author}/#{repo}"
   end
+
+  def flash_class(level)
+    case level
+      when 'notice' then "alert alert-info"
+      when 'success' then "alert alert-success"
+      when 'error' then "alert alert-danger"
+      when 'alert' then "alert alert-warning"
+    end
+  end
 end
