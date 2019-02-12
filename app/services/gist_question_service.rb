6ac970412490024e97b01ebf2f755f83aead4169
ruuke <ruuke@mail.ru>
2019-02-12 10:22:52 +0500

    fix notes from api

--- app/services/gist_question_service.rb
+++ app/services/gist_question_service.rb
@@ -14,7 +14,7 @@ class GistQuestionService
 
   def gist_params
     {
-      description: "A question about #{@test.title} from TestGuru",
+      description: I18n.t('services.gist_params.question_description', title: @test.title),
       files: {
         'test-guru-question.txt' => {
           content: gist_content
--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,5 @@ module TestGuru
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
-    config.autoload_paths << "#{Rails.root}/lib/clients"
   end
 end
