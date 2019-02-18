cd86de38cd220766de06c150c9792a0bcd0d9b23
ruuke <ruuke@mail.ru>
2019-02-18 11:59:34 +0500

    switch to pg

---
+++ db/migrate/20190218065340_add_index_to_catigories.rb
@@ -0,0 +1,4 @@
+class AddIndexToCatigories < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_11_060241) do
+ActiveRecord::Schema.define(version: 2019_02_18_065340) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "answers", force: :cascade do |t|
     t.boolean "correct", default: false
