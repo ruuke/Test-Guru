35ab638a99e0fcb407b5454e7ec45247805ef0b1
ruuke <ruuke@mail.ru>
2019-02-19 11:12:55 +0500

    fix config error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'protests.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settinhs = {
+  config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
     user_name:            ENV['SMTP_USERNAME'],
