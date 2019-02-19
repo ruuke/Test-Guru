76233050eef6f59c502ccf04977fe798dc4c8c17
ruuke <ruuke@mail.ru>
2019-02-19 13:04:28 +0500

    edit seeds

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -4,7 +4,7 @@ class Answer < ApplicationRecord
   scope :correct, -> { where(correct: true) }
 
   validates :body, presence: true
-  validate :validate_numbers_of_answers, on: :create
+  validate :validate_numbers_of_answers, on: :save
 
   def validate_numbers_of_answers
   	message = "кол-во ответов может быть не больше 4-х"
